2c05182bd83a7d51e9e84cfddea0c7898e4ac9bb
Pedro F. Thomaz <pfthomaz@gmail.com>
2016-06-08 14:41:29 +0100

    Rails Tutorial 26 Org Companies, Signing In Users

---
+++ app/controllers/org_companies_controller.rb
@@ -0,0 +1,16 @@
+class OrgCompaniesController < ApplicationController
+
+	def new
+		signed_in_user #Be sure the user is signed in before he can create a company
+		@company = OrgCompany.new # Make new company object 
+		@contactInfo = OrgContact.new.attributes  # Make new contact object w/ empty attributes
+		@company.org_contacts.build(@contactInfo)
+	end
+
+	private
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url, flash: {warning: "Please sign in."}			
+		end
+end
