b12bf79f0d9d7b835054b10fbe80034725b8e8b7
Pedro F. Thomaz <pfthomaz@gmail.com>
2016-06-16 17:07:48 +0100

    Rails Tutorial 39 Positions and Page Access

--- app/controllers/org_people_controller.rb
+++ app/controllers/org_people_controller.rb
@@ -1,4 +1,7 @@
 class OrgPeopleController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update, :stripe_settings]
+  	before_action :correct_user,   only: [:edit, :update]
+
 	def edit
 		# All the setup that is needed to build the edit page view
 		@person = OrgPerson.find(params[:id])
@@ -41,10 +44,59 @@ class OrgPeopleController < ApplicationController
 		end
 	end
 
+	# Edits a position/role (i.e. COO, Director, Store Manager, etc.)
+	def edit_position
+		person_info = OrgPerson.find_by_id(params[:id])
+		person_info.update(typ_position_id: params[:typ_position_id], org_company_id: current_org_person.org_company_id)
+	end
+	# Removes a person from the company
+	def remove_from_company
+		person_info = OrgPerson.find_by_id(params[:id])
+		person_info.update(typ_position_id: 0, org_company_id: nil)
+		contact_info = OrgContact.find_by(org_person_id: params[:id])
+		contact_info.update(org_company_id: nil)
+	end
+
+	# This connects the company to a bank account in their country, only accessible by the COO
+	def stripe_settings
+		@manager = StripeManaged.new(current_org_person)
+		if !current_org_person.stripe_account_status.nil?
+			@charges = [ 'Charges', current_org_person.stripe_account_status['charges_enabled'] ]
+			@transfers = [ 'Transfers', current_org_person.stripe_account_status['transfers_enabled'] ]
+			@dob = @manager.legal_entity.dob
+			@date_selected = Date.new(@dob.year, @dob.month, @dob.day) rescue nil
+		end
+	end
+	# This updates the necessary information required by stripe
+	def stripe_update_settings
+		manager = current_org_person.manager
+		manager.update_account! params: params
+		redirect_to org_people_stripe_settings_path
+	end
 	private 
 		def update_person_params
 			params.require(:org_person).permit(org_contacts_attributes: [:address1, :address2,
         	:city, {typ_countries: :id}, {typ_regions: :id}, {org_company: :id}, :postal_code, :email, 
         	:business_number, :cell_number, :org_person_id, :avatar])
 		end
+
+		# Before filters
+
+	    def signed_in_user
+	      unless signed_in?
+	        store_location
+	        redirect_to signin_url, flash: {warning: "Please sign in."}
+	      end
+	    end
+
+	    # Checks to see if the current user is actually the user this page is suppose to show
+    	# We don't want a person to type in the user of someone else in the link and edit their info
+	    def correct_user
+	      @user = OrgPerson.find_by_id(params[:id]) 
+	      if !@user.nil? && (current_org_person.id == @user.id) #If user is found, go to edit page, else go to sign in
+	      else
+	        flash[:danger] = "The request cannot be fulfilled."
+	        redirect_to(signin_path)
+	      end
+	    end
 end
--- app/models/org_person.rb
+++ app/models/org_person.rb
@@ -1,9 +1,5 @@
 class OrgPerson < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  # Include default devise modules. Others available are:
   # , :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
